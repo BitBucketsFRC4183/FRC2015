119f54a52091e4c10744c6ed33ddf2b889921e2a
roryee <james.wyeth@gmail.com>
2015-01-09 17:01:06 -0700

    Added an interface for the motion profilers.

---
+++ org/bitbuckets/frc2015/lib/MotionProfiler.java
@@ -0,0 +1,25 @@
+package org.bitbuckets.frc2015.lib;
+
+public interface MotionProfiler {
+	/**
+	 * Initializes the <code>MotionProfiler</code>.
+	 * 
+	 * @param pos The target position for the <code>MotionProfiler</code>.
+	 * @param maxVel The max velocity at which the motor should run.
+	 */
+	public void init(int pos, int maxVel);
+	
+	/**
+	 * Gets the current speed at which the motor should be running.
+	 * 
+	 * @return The current speed of this profiler.
+	 */
+	public float getSpeed();
+	
+	/**
+	 * Updates the speed at which the motor should be running.
+	 * 
+	 * @return The current speed to set the motor to.
+	 */
+	public float update();
+}
