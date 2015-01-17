3954acc05bb0f4831b5f7abf8ed93d7a6d2521bb
roryee <james.wyeth@gmail.com>
2015-01-17 07:10:03 -0700

    Started implementing the tilting and actuating of the can grabber.

--- org/bitbuckets/frc2015/subsystems/Grabby.java
+++ org/bitbuckets/frc2015/subsystems/Grabby.java
@@ -1,18 +1,47 @@
 package org.bitbuckets.frc2015.subsystems;
 
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.SpeedController;
+import edu.wpi.first.wpilibj.Talon;
+import org.bitbuckets.frc2015.RobotMap;
+
+import edu.wpi.first.wpilibj.CANTalon;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
  *
  */
 public class Grabby extends Subsystem {
+	/** The motor for rotation the grabber **/
+	SpeedController rotator;
+	DoubleSolenoid grabby;
     
-    // Put methods for controlling this subsystem
-    // here. Call these from Commands.
+	public Grabby(){
+		rotator = new CANTalon(RobotMap.GRAB_TILT_MOTOR);
+		grabby = new DoubleSolenoid(RobotMap.GRABBY_SOLENOID_1, RobotMap.GRABBY_SOLENOID_2);
+		if(rotator instanceof CANTalon){
+			CANTalon canRotator = (CANTalon) rotator;
+		}
+	}
 
     public void initDefaultCommand() {
         // Set the default command for a subsystem here.
         //setDefaultCommand(new MySpecialCommand());
     }
+
+	public void setAngle(){
+		if(rotator instanceof CANTalon){
+			CANTalon canRotator = (CANTalon) rotator;
+//			canRotator.setPID();
+		}
+	}
+
+	public boolean getIsAngleReady(){
+		return true;
+	}
+
+	public void setGrabby(boolean closed){
+		grabby.set(closed ? DoubleSolenoid.Value.kForward : DoubleSolenoid.Value.kReverse);
+	}
 }
 
