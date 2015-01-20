90e28560bcfbcfc80001b1f1b454c1400014ed47
Rodrigo Savage <rodrigosavage@gmail.com>
2015-01-19 19:12:54 -0700

    added code to stacky :)

--- org/bitbuckets/frc2015/subsystems/Stacky.java
+++ org/bitbuckets/frc2015/subsystems/Stacky.java
@@ -1,7 +1,9 @@
 package org.bitbuckets.frc2015.subsystems;
 
 import org.bitbuckets.frc2015.RobotMap;
+import org.bitbuckets.frc2015.Robot;
 
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
@@ -16,14 +18,34 @@ import edu.wpi.first.wpilibj.command.Subsystem;
  */
 public class Stacky extends Subsystem {
     Talon winch;
-    	
+    DigitalInput hallSensors[];
+    int totalHallSensors;
 
     public void initDefaultCommand() {
     	winch = new Talon(RobotMap.WINCH_MOTOR);
+    	totalHallSensors = 5;
+    	hallSensors = new DigitalInput[totalHallSensors];
+    	for(int i=0;i<totalHallSensors;totalHallSensors++){
+    		// Zero is the top one
+    		// creates 5 hallSesors starting digital input 0 from 5
+    		hallSensors[i] = new DigitalInput(i);
+    	}
+    }
+    // returns the number of hooks
+    public int howManyStack(){
+    	int numberOfStacks = 0;
+    	for(int i=0;i<totalHallSensors;totalHallSensors++){
+    		numberOfStacks = numberOfStacks + (hallSensors[i].get()==true?1:0);
+    	}
+    	return numberOfStacks;
     }
     
     public void setWinchMotor(double speed){
     	winch.set(speed);
+    }
+    // constructor
+    public Stacky(){
+    	
     }
 }
 
