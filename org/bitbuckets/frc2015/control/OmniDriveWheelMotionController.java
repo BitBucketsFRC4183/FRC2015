7e35a9e01356cca2fbb2c9f26f8cd49a1e96fee4
roryee <james.wyeth@gmail.com>
2015-01-23 08:17:26 -0700

    Updated the comments again and changed atan to atan2

--- org/bitbuckets/frc2015/control/OmniDriveWheelMotionController.java
+++ org/bitbuckets/frc2015/control/OmniDriveWheelMotionController.java
@@ -25,7 +25,10 @@ public class OmniDriveWheelMotionController implements MotionController {
     }
 
     /**
-     * Gets the speed of a wheel for omni-directional drive with wheels in any configuration. <code>vwp</code> is the velocity vector for the wheel without accounting for the fact that it can't rotate.
+     * Gets the speed of a wheel for omni-directional drive with wheels in any configuration. <code>vwp</code> is the
+     * velocity vector for the wheel without accounting for the fact that it can't rotate. <code>thetaR</code> is the
+     * angle between the center of rotation and the wheel. All angles are relative to standard coordinate axes if looked
+     * at from the top.
      *
      * @param xc    The x coordinate of the intended center of rotation.
      * @param yc    The y coordinate of the intended center of rotation.
@@ -39,7 +42,7 @@ public class OmniDriveWheelMotionController implements MotionController {
      */
     public double getWheelSpeed(double xc, double yc, double xw, double yw, double theta, double vxc, double vyc, double rotc) {
         double vtan = Math.sqrt(Math.pow((xc - xw), 2) + Math.pow((yc - yw), 2)) * rotc;
-        double thetaR = Math.atan((yw - yc) / (xw - xc));
+        double thetaR = Math.atan2((yw - yc), (xw - xc));
         double vwp = Math.sqrt(Math.pow(vtan * Math.cos(thetaR) + vxc, 2) + Math.pow(vtan * Math.sin(thetaR) + vyc, 2));
         double speed = vwp * Math.cos(thetaR - theta);
         return speed;
