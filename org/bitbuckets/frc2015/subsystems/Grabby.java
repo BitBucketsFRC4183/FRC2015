98d8d1d5bb41bca6be238758701fc4d93860ae5a
roryee <james.wyeth@gmail.com>
2015-01-28 16:08:07 -0700

    Removed the grabby tilting motors since we aren't tilting grabby

--- org/bitbuckets/frc2015/subsystems/Grabby.java
+++ org/bitbuckets/frc2015/subsystems/Grabby.java
@@ -1,7 +1,6 @@
 package org.bitbuckets.frc2015.subsystems;
 
 import edu.wpi.first.wpilibj.AnalogPotentiometer;
-import edu.wpi.first.wpilibj.CANTalon;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.SpeedController;
 import edu.wpi.first.wpilibj.command.Subsystem;
@@ -11,11 +10,6 @@ import org.bitbuckets.frc2015.RobotMap;
  *
  */
 public class Grabby extends Subsystem {
-    /**
-     * The motor for rotation of the grabber *
-     */
-    private SpeedController rotator;
-    private SpeedController rotatorAlt;
     /**
      * The motor to control the grabber *
      */
@@ -31,8 +25,6 @@ public class Grabby extends Subsystem {
      * Sets up all the solenoids and motors.
      */
     public Grabby() {
-        rotator = new CANTalon(RobotMap.GRAB_TILT_MOTOR);
-        rotatorAlt = new CANTalon(RobotMap.GRAB_TILT_MOTOR_ALT);
         grabby = new DoubleSolenoid(RobotMap.GRABBY_SOLENOID_1, RobotMap.GRABBY_SOLENOID_2);
         flippy = new DoubleSolenoid(RobotMap.FLIPPY_SOLENOID_1, RobotMap.FLIPPY_SOLENOID_2);
     }
@@ -40,16 +32,6 @@ public class Grabby extends Subsystem {
     public void initDefaultCommand() {
         // Set the default command for a subsystem here.
         //setDefaultCommand(new MySpecialCommand());
-    }
-
-    /**
-     * Sets the speed of the grabber tilt motor
-     *
-     * @param speed is between -1.0 and 1.0
-     */
-    public void setSpeed(double speed) {
-        rotator.set(speed);
-        rotatorAlt.set(-speed);
     }
 
     /**
