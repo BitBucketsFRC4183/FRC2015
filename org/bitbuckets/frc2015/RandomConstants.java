434977fe2d202d78b909f5fcd9a254133f04f5b9
roryee <james.wyeth@gmail.com>
2015-02-17 17:21:59 -0700

    Changed drive speed

--- org/bitbuckets/frc2015/RandomConstants.java
+++ org/bitbuckets/frc2015/RandomConstants.java
@@ -19,7 +19,7 @@ public class RandomConstants {
     /**
      * Maximum translational speed in ft/s.(needs to be converted to ft/s)
      */
-    public static final double MAX_TRANS_SPEED = 10;
+    public static final double MAX_TRANS_SPEED = 4;
     /**
      * Maximum translational acceleration in ft/s^2.(needs to be converted to ft/s^2)
      */
