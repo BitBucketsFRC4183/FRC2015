22c1db1f0d0759e65c007b95d46bd8f564d712da
milesmarchant <miles@marchant.net>
2015-02-27 16:08:34 -0700

    Wrote autonomous program for picking up the three yellow totes.

--- org/bitbuckets/frc2015/autonomous/ThreeTotePickupAutoMode.java
+++ org/bitbuckets/frc2015/autonomous/ThreeTotePickupAutoMode.java
@@ -1,5 +1,11 @@
 package org.bitbuckets.frc2015.autonomous;
 
+import org.bitbuckets.frc2015.RandomConstants;
+import org.bitbuckets.frc2015.command.StackyDownAll;
+import org.bitbuckets.frc2015.command.StackyUp;
+import org.bitbuckets.frc2015.command.Wait;
+import org.bitbuckets.frc2015.command.autonomous.DrivePolar;
+
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
 /**
@@ -8,6 +14,32 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 public class ThreeTotePickupAutoMode extends CommandGroup {
 
     public ThreeTotePickupAutoMode() {
+    	
+    	//pick up first tote
+    	addParallel(new StackyUp());
+    	addSequential(new Wait(0.1));
+    	addSequential(new DrivePolar(0.5, 180, RandomConstants.MAX_TRANS_SPEED));
+    	addSequential(new Wait(0.1));
+    	//move to second tote
+    	addSequential(new DrivePolar(6.846, 270, RandomConstants.MAX_TRANS_SPEED));
+    	addSequential(new Wait(0.1));
+    	addSequential(new DrivePolar(0.5, 0, RandomConstants.MAX_TRANS_SPEED));
+    	addSequential(new Wait(0.1));
+    	//pick up second tote
+    	addParallel(new StackyUp());
+    	addSequential(new Wait(0.1));
+    	addSequential(new DrivePolar(0.5, 180, RandomConstants.MAX_TRANS_SPEED));
+    	addSequential(new Wait(0.1));
+    	//move to third tote
+    	addSequential(new DrivePolar(6.846, 270, RandomConstants.MAX_TRANS_SPEED));
+    	addSequential(new Wait(0.1));
+    	//push third tote into autozone
+    	addSequential(new DrivePolar(9, 180, RandomConstants.MAX_TRANS_SPEED));
+    	addSequential(new Wait(0.1));
+    	//set down all totes
+    	addSequential(new StackyDownAll());
+    	//drive backwards a little bit to not be touching the totes
+    	addSequential(new DrivePolar(0.5, 180, RandomConstants.MAX_TRANS_SPEED));
 
     	/*
     	 * Things that need to happen during this autonomous:
