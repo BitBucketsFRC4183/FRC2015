0828beee45c33c0511dcf55b8dc6e6584dc20cf1
milesmarchant <miles@marchant.net>
2015-04-08 15:48:12 -0700

    .

--- org/bitbuckets/frc2015/Robot.java
+++ org/bitbuckets/frc2015/Robot.java
@@ -6,6 +6,7 @@ import org.bitbuckets.frc2015.autonomous.AutoDriveTest;
 import org.bitbuckets.frc2015.autonomous.DriveToAutoZone;
 import org.bitbuckets.frc2015.autonomous.ThreeTotePickupAutoMode;
 import org.bitbuckets.frc2015.command.CanStepGrab;
+import org.bitbuckets.frc2015.command.LowLatencyDrive;
 import org.bitbuckets.frc2015.command.StackyDown;
 import org.bitbuckets.frc2015.command.StackyDownAll;
 import org.bitbuckets.frc2015.command.StackyMoveDistance;
@@ -163,8 +164,9 @@ public class Robot extends IterativeRobot {
 //        drivey.setEncoderSetting(ControlMode.Position);
         SerialPortManager.analogGyro.reset();
         
-//        autonomousCommand = new CanStepGrab(0.5, 500, true);
-        autonomousCommand = (Command) new AutoCanRetrieval();
+        autonomousCommand = new CanStepGrab(0.5, 500, true, "test");
+//        autonomousCommand = (Command) new AutoCanRetrieval();
+//        autonomousCommand = new LowLatencyDrive();
 
         //autonomousCommand = (Command) autoChooser.getSelected();
         if(autonomousCommand != null){
